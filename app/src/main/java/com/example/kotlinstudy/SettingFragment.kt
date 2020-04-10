5f8007e3f828e39c5ae4d98ea3a82908fe80d7f9
ChaMinzi <ckalswl331@naver.com>
2020-04-11 00:05:42 +0900

    Fix Home Fragment UI

--- app/src/main/java/com/example/kotlinstudy/SettingFragment.kt
+++ app/src/main/java/com/example/kotlinstudy/SettingFragment.kt
@@ -1,4 +1,28 @@
 package com.example.kotlinstudy
 
-class SettingFragment {
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+class SettingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+
+        return true
+    }
 }
