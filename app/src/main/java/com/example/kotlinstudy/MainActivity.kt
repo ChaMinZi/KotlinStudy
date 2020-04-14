e5f365fa337c9c4170567e01f99aebd43688f7de
ChaMinzi <ckalswl331@naver.com>
2020-04-14 23:54:38 +0900

    Add BottomNavigationView flow

--- app/src/main/java/com/example/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.kotlinstudy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_service_center)
+        setContentView(R.layout.activity_main)
+        
+        val host : NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        var navHostController = host.navController
+        navHostController = findNavController(R.id.nav_host_fragment)
+
+        NavigationUI.setupWithNavController(bottom_navigation_view, navHostController)
     }
 }
--- app/src/main/java/com/example/kotlinstudy/PromotionFragment.kt
+++ app/src/main/java/com/example/kotlinstudy/PromotionFragment.kt
@@ -1,4 +1,19 @@
 package com.example.kotlinstudy
 
-class PromotionFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PromotionFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_promotion, container, false)
+
+        return view
+    }
 }
